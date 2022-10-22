f395084bb697a8d454cdcc4841791f65774f915b
petro_thomas <peter88tom@gmail.com>
2022-10-22 02:02:15 +0200

    Save user information to database

--- app/forms.py
+++ app/forms.py
@@ -6,7 +6,8 @@ class CustomerInformationForm(forms.ModelForm):
   first_name = forms.CharField(max_length=200, required=True)
   last_name = forms.CharField(max_length=200, required=True)
   date_of_birth = forms.CharField(max_length=200, required=True)
+  uploaded_file = forms.FileField(required=True)
 
   class Meta:
     model = CustomerInformation
-    fields = ('first_name', 'last_name', 'date_of_birth')
+    fields = ('first_name', 'last_name', 'date_of_birth', 'uploaded_file')
--- app/views.py
+++ app/views.py
@@ -1,6 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from django.contrib import messages
+from app.forms import CustomerInformationForm
 
 # Create your views here.
 def index(request):
+  if request.method == 'POST':
+    form = CustomerInformationForm(request.POST, request.FILES)
+    if form.is_valid():
+      print("Form is valid")
+      form.save()
+    else:
+      print("Form is not valid")
+      messages.warning(request, 'Form is not valid')
+      return redirect('/')
+
   return render(request, 'index.html')
